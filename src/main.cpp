74e33aaa369df7e74cde0d9a60ba0cc940640282
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-03-11 16:59:27 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <cmath>
 
 int main() {
     return 0;
